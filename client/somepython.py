23bd63656b1b5b79d1ce49e6a9f8e76a069af363
azoth zephyr <azoth@zephyr.sx>
2023-11-18 13:16:07 -0800

    remove old scripts / data and create scripts folder for arbitrage / flashloan arbitrage scripts:

--- client/somepython.py
+++
@@ -1,8 +0,0 @@
-import json 
-with open ('mydata.csv', 'r') as f: 
-    loaded_json = json.load(f)
-anarray = []
-for value in loaded_json['result']:
-    anarray.append(value['pubkey'])
-with open ('mydata.json', 'w') as f: 
-    f.write(json.dumps(anarray))
